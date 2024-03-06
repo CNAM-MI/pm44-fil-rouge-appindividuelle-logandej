5ac1b1470fb6417cb766e80068988fb159d06731
logandej <74404279+logandej@users.noreply.github.com>
2024-03-06 13:16:00 +0100

    Ajoutez des fichiers projet.

---
+++ MyFirstMauiApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+namespace MyFirstMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        BigInteger count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ChangeName("Je fais du Java");
+        }
+
+        private void ChangeName(string name)
+        {
+            LabelName.Text = name;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            if(count == 0)
+            {
+                count++;
+            }
+            else
+            {
+                count *= 2;
+            }
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
